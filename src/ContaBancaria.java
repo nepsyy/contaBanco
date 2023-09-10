3d71379f82ab3844a3c20aa37629185411ee85d4
webnep <wesleyuuu10@gmail.com>
2023-09-10 14:29:06 -0400

    commit

---
+++ src/ContaBancaria.java
@@ -0,0 +1,15 @@
+public class ContaBancaria {
+    public static void main(String[] args) {
+        int numero = Integer.valueOf(args [3]);
+        String agencia = args [2];
+        String nomeCliente = args [0];
+        String sobrenomeCliente = args [1];
+        double saldo = Integer.valueOf(args [4]);
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + 
+        ", seja bem-vindo ao nosso banco. Sua agência é " + agencia + 
+        ", conta " + numero + " e seu saldo " + saldo + 
+        " já está disponível para saque.");
+        }
+    
+}
